21fbca365d56b5dd7b341da247083a638f90dc80
cjb2272 <cjb2272@g.rit.edu>
2025-03-10 23:11:13 -0400

    spotify proj section

--- frontend/src/components/MainSections/ProjectSections/SpotifyProj.js
+++ frontend/src/components/MainSections/ProjectSections/SpotifyProj.js
@@ -1,21 +1,27 @@
+import React from 'react';
+import testPlaylistImg from '../../../assets/images/test-chance-playlist-spotify-screenshot.webp'; //4:3
+
 const SpotifyProj = () => {
     return (
       <>
         <h1 className="text-2xl font-bold pb-2">Playlist Automation with ChatGPT</h1>
         <p className="written-content-centering-width mb-2">I am currently working on a Flask Application leveraging the OpenAI API in tandem 
           with Spotify&apos;s Web API in order to automate various procedures within the streaming service. </p>
-        <p className="written-content-centering-width mb-2">PASTE CONTENT IMAGE/VIDEO DEMO HERE </p>
-        <p className="written-content-centering-width mb-2">Rolling down the street smoking endo, laid back. Optimizing dime bags of pre-rolled 
-          honey oil dabs. Fully man, keif gummies are the indoor equivalent of body high super mellow. Have you ever 
-          tried Lorem Ipsum on Weeeeeeeeeeed? Rasta!</p>
-        <p className="written-content-centering-width mb-2">Legalize spliffs for recreational Snoop Dogg edibles pusher cannabidiol cartoons.
-          Guatemalan purple haze grown outdoors by ganja shaman. Broccoli bong butterfly gateway drugs to elevate your
-           good vibrations. Pass the duchie Bob Marley this shatter is hella potent. Wake and bake indica hash at the
-            dispensary in Oregon decriminalized.</p>
-        <p className="written-content-centering-width">Legalize spliffs for recreational Snoop Dogg edibles pusher cannabidiol cartoons.
-          Guatemalan purple haze grown outdoors by ganja shaman. Broccoli bong butterfly gateway drugs to elevate your
-           good vibrations. Pass the duchie Bob Marley this shatter is hella potent. Wake and bake indica hash at the
-            dispensary in Oregon decriminalized.</p>
+        <div className='mx-auto max-w-xl p-4'>
+          <div className="aspect-w-16- aspect-h-10 rounded-lg overflow-hidden">
+            <img src={testPlaylistImg} alt="Spotify Screenshot of Automated Playlist" className="w-full h-full object-cover"/>
+          </div>
+        </div>
+        <p className="written-content-centering-width mb-2">I got the idea for utilizing the OpenAI API for automating Spotify tasks from 
+          an RIT AI Club meeting. I was highly intrigued right away because I have always wanted to index the entire catalog of songs 
+          available on Spotify for songs that include my favorite artist Chance the Rapper. In doing this, here are a few subjects I am 
+          currently learning and practicing in order to implement a perfect solution.</p>
+        <ul className="list-disc written-content-centering-width pl-10">
+          <li className>Routing with Flask</li>
+          <li className>Auth. Flow (Spotify API authentication)</li>
+          <li className>OpenAI API "Function Calling" configuration</li>
+          <li className>Structured Output prompting</li>
+        </ul>
       </>
     );
   };
