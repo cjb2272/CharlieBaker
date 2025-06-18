b730752a723d89c5b52cc81d5ec25a2e772b812f
cjb2272 <cjb2272@g.rit.edu>
2025-06-18 16:06:16 -0400

    tiny text edit

--- frontend/src/components/MainSections/ProjectSections/SpotifyProj.js
+++ frontend/src/components/MainSections/ProjectSections/SpotifyProj.js
@@ -5,8 +5,8 @@ const SpotifyProj = () => {
   return (
     <>
       <h1 className="text-2xl font-bold pb-2">Playlist Automation with ChatGPT</h1>
-      <p className="written-content-centering-width mb-2">I am currently working on a Flask application leveraging the OpenAI API in tandem
-        with Spotify&apos;s Web API in order to automate various procedures within the streaming service. </p>
+      <p className="written-content-centering-width mb-2">Flask application leveraging the OpenAI Chat Completions API in tandem
+        with Spotify&apos;s Web API in order to automate various procedures within the streaming service.</p>
       <div className='mx-auto max-w-xl p-4'>
         <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
           <img src={testPlaylistImg} alt="Spotify Screenshot of Automated Playlist" className="w-full h-full object-cover" />
@@ -14,8 +14,8 @@ const SpotifyProj = () => {
       </div>
       <p className="written-content-centering-width mb-2">I got the idea for utilizing the OpenAI API for automating Spotify tasks from
         an RIT AI Club meeting. I was highly intrigued right away because I have always wanted to index the entire catalog of songs
-        available on Spotify for songs featuring my favorite artist Chance the Rapper. In doing this, here are a few subjects I am
-        currently learning and practicing in order to implement a perfect solution.</p>
+        available on Spotify for songs featuring my favorite artist Chance the Rapper. In doing this, here are a few subjects I am learning 
+        and practicing.</p>
       <ul className="list-disc written-content-centering-width pl-10">
         <li className>Routing with Flask</li>
         <li className>Auth. Flow (Spotify API authentication)</li>
