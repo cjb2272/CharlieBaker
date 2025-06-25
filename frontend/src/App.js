6f55b390a7815cc10bfedc079983387bc601d41f
cjb2272 <cjb2272@g.rit.edu>
2025-06-25 16:31:24 -0400

    beginning to add chatbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,12 @@
  * the components folder is a container for all additional components that will be used in the app.
  */
 import './App.css';
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
 import SideBarNavigation from './components/SideBarNavigation.js';
 import ScrollingPage from './components/ScrollingPage.js';
+
 
 /**
  * 
@@ -27,6 +30,25 @@ const App = () => {
   // Without useCallback, a new instance of the setActiveSection function 
   // would be created on every re-render of App.
   // which would inturn re-render the SideBarNavigation component and the ScrollingPage component.
+
+  useEffect(() => {
+		createChat({
+			webhookUrl: process.env.REACT_APP_PERSONAL_SITE_CHATBOT_N8N_WEBHOOK_URL,
+      initialMessages: [
+		    'Hey, Its Charlie! (not really) 🤣',
+		    'My name is CharlieBot. Whats up?'
+	    ],
+	    i18n: {
+		    en: {
+			    title: '',
+			    subtitle: "Start a chat with CharlieBot below.",
+			    footer: '',
+			    getStarted: 'New Conversation',
+			    inputPlaceholder: 'Type your question..',
+		    },
+	    }
+		});
+	}, []);
 
   return (
     <>
